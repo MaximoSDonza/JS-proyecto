df6aa8cd50495b3deb554366f96277f44d287358
Donza <maximosebastiandonza@gmail.com>
2024-03-14 01:02:26 -0300

    La calculadora esta en estado funcional

--- calculadora.js
+++ calculadora.js
@@ -1,17 +1,20 @@
 function pantalla(valor){
     document.getElementById('pantallita').value += valor;
-
     return
 }
 
 function borrarTodo(){
-    document.getElementById('pantallita').value = ''
+    document.getElementById('pantallita').value = null
     return
 }
 
+function borrar(){
+    document.getElementById('pantallita').value = document.getElementById('pantallita').value.slice(-1)
+}
+
 function calcular(){
-    const valorPantalla = documment.getElementById('pantallita').value
-    const resultado = eval(valorPantalla)
-    documment.getElementById('pantallita').value = resultado
+    const valorPantalla= document.getElementById('pantallita').value
+    const resultado= eval(valorPantalla)
+    document.getElementById('pantallita').value = resultado
     return
 }
